1e8232858a0355b12955206ff3d46dfb710d8925
Vincent San Joaquin <vincent.sanjoaquin@techfino.com>
2025-10-20 04:47:48 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const API = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:3001';
+
+module.exports = {
   async rewrites() {
-    return [{ source: '/api/:path*', destination: 'http://127.0.0.1:3001/:path*' }];
+    return [{ source: '/api/:path*', destination: `${API}/:path*` }];
   },
 };
-module.exports = nextConfig;
